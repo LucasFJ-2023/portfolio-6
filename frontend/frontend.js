d730f9e00153c43f0c3d99f4fdacc29fe08533af
vgn40 <victornylev@hotmail.dk>
2023-12-05 21:51:52 +0100

    Har fået lidt hul igennem til cafe.html og index.html

--- frontend/frontend.js
+++ frontend/frontend.js
@@ -2,11 +2,10 @@
 // victor
 document.addEventListener('DOMContentLoaded', () => {
     const cafeImagesContainer = document.getElementById('cafeImages');
-
-    fetch('/cafe/:id')
+    fetch('http://localhost:3000/randomcafes')
         .then(response => {
             if (!response.ok) {
-                throw new Error('Network response was not ok');
+                throw new Error('Not ok');
             }
             return response.json();
         })
@@ -14,15 +13,17 @@ document.addEventListener('DOMContentLoaded', () => {
             cafes.forEach(cafe => {
                 const cafeElement = document.createElement('div');
                 cafeElement.innerHTML = `
+                    <img src="${cafe.img_url}">
                     <h3>${cafe.cafe_name}</h3>
                     <p>Address: ${cafe.address}, ${cafe.city}</p>
                     <p>Description: ${cafe.description}</p>
+                    
                 `;
                 cafeImagesContainer.appendChild(cafeElement);
             });
         })
         .catch(error => {
-            console.error('Error fetching random cafes:', error);
+            console.error('cant fetch cafes:', error);
         });
 });
 
