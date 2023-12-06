4d5835f8bdde2105301ed891a705b581cbda8a6a
vgn40 <victornylev@hotmail.dk>
2023-12-06 14:57:33 +0100

    Victor - Oprettet funktionalitet som check i databasen om cafer sælger mad og om de har wifi

--- frontend/frontend.js
+++ frontend/frontend.js
@@ -41,36 +41,20 @@ document.addEventListener('DOMContentLoaded', () => {
         .then(cafe => {
             const cafeImageContainer = document.getElementById('cafeImageContainer');
             const cafeInformationContainer = document.getElementById('cafeInformationContainer');
+            const wifiImage = document.querySelector('.true-or-false-wifi');
+            const foodImage = document.querySelector('.true-or-false-food');
 
             if (cafe) {
                 cafeImageContainer.innerHTML = `<img src="${cafe[0].img_url}">`;
                 cafeInformationContainer.innerHTML = `<h2>${cafe[0].cafe_name}</h2> <p>Address: ${cafe[0].address}, ${cafe[0].city}</p><p>Description: ${cafe[0].description}</p>`;
+                wifiImage.src = cafe[0].wifi ? './img/istrue.png' : './img/isfalse.png';
+                foodImage.src = cafe[0].serve_food ? './img/istrue.png' : './img/isfalse.png';
             }
+
         })
         .catch(error => {
             console.error('Error fetching cafe details:', error);
         });
 });
 
-document.addEventListener('DOMContentLoaded', () => {
-    fetch(`http://localhost:3000/cafe/`)
-        .then(response => {
-            if (!response.ok) {
-                throw new Error('Failed to fetch');
-            }
-            return response.json();
-        })
-        .then(cafe => {
-            const cafeImageContainer = document.getElementById('cafeImageContainer');
-            const cafeInformationContainer = document.getElementById('cafeInformationContainer');
 
-            if (cafe) {
-                cafeImageContainer.innerHTML = `<img src="${cafe[0].img_url}">`;
-                cafeInformationContainer.innerHTML = `<h2>${cafe[0].cafe_name}</h2> <p>Address: ${cafe[0].address}, ${cafe[0].city}</p><p>Description: ${cafe[0].description}</p>`;
-            }
-        })
-        .catch(error => {
-            console.error('Error fetching cafe details:', error);
-        });
-});
-src=""
